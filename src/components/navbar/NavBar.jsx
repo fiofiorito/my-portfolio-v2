1052931089fe26b2047e91bbad4ed5e10293dc47
Fiorella <124637724+fiofiorito@users.noreply.github.com>
2023-10-23 12:28:50 -0300

    navbar fully responsive

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,16 +1,43 @@
+import { useState } from "react";
 import "./NavBar.css";
 
 const NavBar = () => {
-    return <nav className="navbar">
+    const menuIcon = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+    const [menuIconClass, setMenuIconClass] = useState("navbar-menu-icon unclicked");
+    const [isMenuClicked, setIsMenuClicked] = useState(false);
+    const [navbarListClass, setNavbarListClass] = useState("navbar-list-div hidden");
+
+    const displayMenu = () => {
+        if (!isMenuClicked) {
+            setMenuIconClass("navbar-menu-icon clicked");
+            setNavbarListClass("navbar-list-div visible");
+        } else {
+            setMenuIconClass("navbar-menu-icon unclicked");
+            setNavbarListClass("navbar-list-div hidden");
+        }
+        setIsMenuClicked(!isMenuClicked);
+    }
+    return <div className="navbar">
         <div className="navbar-logo">
-            <p>logo</p>
+            <p className="navbar-logo-p">iFio</p>
         </div>
-        <ul className="navbar-list">
-            <li className="navbar-item">navbar</li>
-            <li className="navbar-item">navbar</li>
-            <li className="navbar-item">navbar</li>
-        </ul>
-    </nav>
+        <div className="navbar-menu" onClick={displayMenu}>
+            {
+                menuIcon.map((icon) => {
+                    return <div className={menuIconClass} key={icon}></div>
+                })
+            }
+        </div>
+
+        <nav className={navbarListClass}>
+            <ul className="navbar-list">
+                <li className="navbar-item">navbar</li>
+                <li className="navbar-item">navbar</li>
+                <li className="navbar-item">navbar</li>
+            </ul>
+        </nav>
+    </div>
+
 }
 
 export default NavBar;
