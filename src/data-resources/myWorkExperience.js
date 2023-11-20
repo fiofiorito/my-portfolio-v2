021aacb7475089f48e2ce6d6801ac92732f3cfb8
Fiorella <124637724+fiofiorito@users.noreply.github.com>
2023-11-20 16:35:02 -0300

    add gifs on work experience tab

--- src/data-resources/myWorkExperience.js
+++ src/data-resources/myWorkExperience.js
@@ -14,7 +14,7 @@ export const myWorkExperience = [
         role: "Social media managment",
         date: "05-2023 to present",
         description: "The company's Instagram account has grown +8% on their followers and interaction has grown more than 30%. My job is to plan out the content that gets posted each month with the intention of selling the company's 1:1 program.",
-        img: "https://res.cloudinary.com/drhwvqo2m/image/upload/v1700233175/portfolio-2v/tziid9tr6jqsbrbuk6h6.png",
+        img: "src/visuals/gif-socials.gif",
         imgAlt: "My job as a social media manager at Fitness Finaciero"
     },
     {
@@ -23,10 +23,11 @@ export const myWorkExperience = [
         role: "Video Editing Freelancer",
         date: "02-2023 to present",
         description: "I offer my video editing services to different clients around the world. I have edited more than 50 videos for my different clients so far! My services range from social media video editing to podcast video and audio editing.",
-        img: "https://res.cloudinary.com/drhwvqo2m/image/upload/v1700233175/portfolio-2v/tziid9tr6jqsbrbuk6h6.png",
+        img: "src/visuals/gif-freelance.gif",
         imgAlt: "My job as a freelancer"
     }
 ]
+
 
 export const workData = {
     img: "https://res.cloudinary.com/drhwvqo2m/image/upload/v1700234581/portfolio-2v/kxrqil3wzisspyn6sodl.jpg",
